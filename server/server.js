e9b2a079290c9fb2e5cfe442b117bac6ecea2a59
Emiliano Martino <emiliano.martino@devborn.com>
2025-07-07 00:21:22 -0300

    added cors validation

--- server/server.js
+++ server/server.js
@@ -47,7 +47,23 @@ const app = express();
 const PORT = process.env.PORT || 5001;
 
 // Middleware
-app.use(cors());
+const allowedOrigins = [
+  "http://localhost:3000",
+  "https://crypto-dashboard-metrics-bice.vercel.app/"
+];
+
+app.use(
+  cors({
+    origin: (origin, callback) => {
+      if (!origin || allowedOrigins.includes(origin)) {
+        callback(null, true);
+      } else {
+        callback(new Error("Not allowed by CORS"));
+      }
+    },
+    credentials: true,
+  })
+);
 app.use(bodyParser.json());
 
 // Ruta proxy para BitMEX
